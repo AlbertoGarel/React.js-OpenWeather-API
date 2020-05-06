7738c80dfb90f4bd26fc8456fb9fcd27add5a17d
AlbertoGarel <qmelias@gmail.com>
2020-05-06 19:34:15 +0200

    Added Formulario and Header Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
-    <div>
-      <h1>...prueba...</h1>
-    </div>
+    <Fragment>
+      <Header titulo="Clima React App" />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
